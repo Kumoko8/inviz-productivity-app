e27ff890d643b338489a49170aac410c6852c88d
Kumori <tauen8@gmail.com>
2025-11-07 16:52:10 -0600

    added more animation for 3 more characters

--- frontend/src/components/CharacterData.tsx
+++ frontend/src/components/CharacterData.tsx
@@ -1,9 +1,9 @@
 import Kumo3 from '../assets/Kumo_3.jpg'
-import Kiiro4 from '../assets/Kiiro_4.jpg'
-import Jack4 from '../assets/Jack_4.jpg'
-import Maguro5 from '../assets/Maguro_5.jpg'
-import Lucas4 from '../assets/Lucas_4.jpg'
+import KiiroVideo from '../assets/Kiiro_Run.mp4'
+import MaguroVid from '../assets/Maguro.mp4'
+import LucasVid from '../assets/Lucas.mp4'
 import HalVideo from '../assets/HalVid.mp4'
+import YumiVideo from '../assets/Yumi_Run.mp4'
 
 const characters = [
     {
@@ -14,27 +14,33 @@ const characters = [
     },
     {
         name: 'Kiiro',
-        baseImages: [Kiiro4],
-        confirmedImages: [Kiiro4],
-        animation: [2, 5, 10, 20, 40], 
+        baseImages: [ ],
+        confirmedImages: [ ],
+        animation: [KiiroVideo], 
     },
     {
         name: 'Maguro',
-        baseImages: [Maguro5],
-        confirmedImages: [Maguro5],
-        animation: [2, 4, 8, 15, 30], // Thresholds for Maguro's progression
+        baseImages: [ ],
+        confirmedImages: [ ],
+        animation: [MaguroVid], // Thresholds for Maguro's progression
     },
     {
         name: 'Lucas',
-        baseImages: [Lucas4],
+        baseImages: [ ],
         confirmedImages: [ ],
-        animation: [2, 4, 8, 15, 30], // Thresholds for Lucas's progression
+        animation: [LucasVid], // Thresholds for Lucas's progression
     },
     {
         name: 'Hal',
         baseImages: [ ],
         confirmedImages: [ ],
         animation: [HalVideo], // Thresholds for Jack's progression
+    },
+    {
+        name: 'Yumi',
+        baseImages: [ ],
+        confirmedImages: [ ],
+        animation: [YumiVideo], // Thresholds for Jack's progression
     },
 ];
 
