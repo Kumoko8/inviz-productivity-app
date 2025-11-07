0851ba74c54935ad040091f3d87248e099b86a2d
Kumori <tauen8@gmail.com>
2025-11-07 08:41:40 -0600

    successful test of Jack animation

--- frontend/src/components/CharacterData.tsx
+++ frontend/src/components/CharacterData.tsx
@@ -3,37 +3,38 @@ import Kiiro4 from '../assets/Kiiro_4.jpg'
 import Jack4 from '../assets/Jack_4.jpg'
 import Maguro5 from '../assets/Maguro_5.jpg'
 import Lucas4 from '../assets/Lucas_4.jpg'
+import JackVideo from '../assets/test.mp4'
 
 const characters = [
     {
         name: 'Kumo',
         baseImages: [Kumo3],
         confirmedImages: [Kumo3],
-        thresholds: [5, 8, 12, 20, 35], // Thresholds for Kumo's progression
+        animation: [5, 8, 12, 20, 35], // Thresholds for Kumo's progression
     },
     {
         name: 'Kiiro',
         baseImages: [Kiiro4],
         confirmedImages: [Kiiro4],
-        thresholds: [2, 5, 10, 20, 40], 
+        animation: [2, 5, 10, 20, 40], 
     },
     {
         name: 'Maguro',
         baseImages: [Maguro5],
         confirmedImages: [Maguro5],
-        thresholds: [2, 4, 8, 15, 30], // Thresholds for Maguro's progression
+        animation: [2, 4, 8, 15, 30], // Thresholds for Maguro's progression
     },
     {
         name: 'Lucas',
         baseImages: [Lucas4],
         confirmedImages: [ ],
-        thresholds: [2, 4, 8, 15, 30], // Thresholds for Lucas's progression
+        animation: [2, 4, 8, 15, 30], // Thresholds for Lucas's progression
     },
     {
         name: 'Jack',
         baseImages: [Jack4],
         confirmedImages: [ ],
-        thresholds: [2, 4, 8, 15, 30], // Thresholds for Jack's progression
+        animation: [JackVideo], // Thresholds for Jack's progression
     },
 ];
 
