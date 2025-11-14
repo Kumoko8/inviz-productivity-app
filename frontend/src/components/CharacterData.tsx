f8059e40930599964ca81fffe5d394ddf13bcdd8
Kumori <tauen8@gmail.com>
2025-11-14 05:12:04 -0600

    moved all animations to firebase

--- frontend/src/components/CharacterData.tsx
+++ frontend/src/components/CharacterData.tsx
@@ -1,9 +1,3 @@
-import KumoVideo from '../assets/Kumo.mp4'
-import KiiroVideo from '../assets/Kiiro.mp4'
-import MaguroVid from '../assets/Maguro.mp4'
-import LucasVid from '../assets/Lucas.mp4'
-import HalVideo from '../assets/Hal.mp4'
-import YumiVideo from '../assets/Yumi.mp4'
 
 export interface Character {
     id: string;
@@ -17,37 +11,37 @@ const characters = [
     {
         id: "kumo_1",
         name: 'Kumo',
-        defaultAnimation: [KumoVideo],
+        defaultAnimation: [],
         animation: 'characters/kumo_1.mp4', // Thresholds for Kumo's progression
     },
     {
         id: "kiiro_1",
         name: 'Kiiro',
-        defaultAnimation: [KiiroVideo],
+        defaultAnimation: [],
         animation: '', 
     },
     {
         id: "maguro_1",
         name: 'Maguro',
-        defaultAnimation: [MaguroVid],
+        defaultAnimation: [],
         animation: [], // Thresholds for Maguro's progression
     },
     {
         id: "lucas_1",
         name: 'Lucas',
-        defaultAnimation: [LucasVid],
+        defaultAnimation: [],
         animation: [], // Thresholds for Lucas's progression
     },
     {
         id: "hal_1",
         name: 'Hal',
-        defaultAnimation: [HalVideo],
+        defaultAnimation: [],
         animation: [], // Thresholds for Jack's progression
     },
     {
         id: "yumi_1",
         name: 'Yumi',
-        defaultAnimation: [YumiVideo],
+        defaultAnimation: [],
         animation: [], // Thresholds for Jack's progression
     },
     {
