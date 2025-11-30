e19c7cd047186e2c37c925a9fc469be625211621
Taylor Golden <131223690+Kumoko8@users.noreply.github.com>
2025-11-30 07:57:58 -0600

    Refactor Firebase config to use environment variables

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBV143OBBajnbZWj-PZPYq0NzWiCqRYp90",
-  authDomain: "inviz-ae0c0.firebaseapp.com",
-  projectId: "inviz-ae0c0",
-  storageBucket: "inviz-ae0c0.firebasestorage.app",
-  messagingSenderId: "25276007249",
-  appId: "1:25276007249:web:f0565c5f6c2cbbca7b4970",
-  measurementId: "G-X0XT5VMPBX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: 
+  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASURMENT_ID as string,
 };
 
 const app = initializeApp(firebaseConfig);
